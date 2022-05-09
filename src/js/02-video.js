f0fe3063cbfbefe0ebff3f3fad1d07f077ec765d
Anton <didenkofficial@gmail.com>
2022-05-09 23:04:09 +0300

    did the second job

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+const throttle = require('lodash.throttle');
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+player.on(
+  'timeupdate',
+  throttle(item => {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(item));
+  }, 1000),
+);
+
+const getSeconds = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+
+player.setCurrentTime(getSeconds.seconds);
